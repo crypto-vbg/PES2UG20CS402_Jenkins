400d74c1529541a647590e13805f6976c86eb065
Vijith BG <79413083+crypto-vbg@users.noreply.github.com>
2023-02-16 09:11:16 +0530

    Add files via upload

---
+++ main/hello.cpp
@@ -0,0 +1,11 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "I have successfully built and run\n";
+  cout << "My SRN: PES2UG20CS402, Name: VIJITH BG";
+  return 0;
+}
